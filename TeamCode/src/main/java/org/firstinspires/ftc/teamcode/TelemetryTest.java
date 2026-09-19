1dec2cfea6f85572dd5737435124b980e80ff043
hemmithundhanasekaran-lab <hemmithun.dhanasekaran@gmail.com>
2026-09-18 19:43:43 -0500

    first java class. This is a telemetry going on the Driver Hub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+;
+@TeleOp
+public class TelemetryTest extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Decagonators");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
