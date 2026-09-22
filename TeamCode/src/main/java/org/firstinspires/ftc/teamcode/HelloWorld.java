0b59a42353ce4489e700fc07889c2a9eabadc3d8
Dhanya <reachdms21@gmail.com>
2026-09-22 17:36:25 -0500

    testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -3,11 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@TeleOp(name = "Hello World")
+@SuppressWarnings("unused")
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");//Testing
+        telemetry.addData("Hello", "World");
     }
 
     @Override
